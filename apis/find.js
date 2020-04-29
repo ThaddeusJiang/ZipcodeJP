945deaa3329b7ef8a7feb38ea855918a5828f3b5
TJ <thaddeusjiang@gmail.com>
2020-04-29 15:43:39 +0900

    improvement(api find): support -

--- apis/find.js
+++ apis/find.js
@@ -2,7 +2,7 @@ const codes = require('../data/codes.json');
 
 const find = (code) => {
   if (code) {
-    const result = codes[code];
+    const result = codes[code.replace(/-/, '')];
     return result;
   }
   return null;
